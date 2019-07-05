760c061c4159a87b0f94bac5b5b7635794a62ee1
Dmitry Misharov <quarckster@gmail.com>
2019-07-05 15:02:49 +0200

    Openshift adapting

--- devpi-entry.sh
+++ devpi-entry.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 if [ ! -f .serverversion ]
 then 
-	devpi-server --serverdir . --init
+	devpi-server --serverdir . \
+				--init \
+				--storage pg8000:host=$DB_HOST,port=$DB_PORT,database=$DB_NAME,user=$DB_USER,password=$DB_PASSWORD
 fi
 
-devpi-server --serverdir . --host=0.0.0.0
+devpi-server --serverdir . --host=0.0.0.0 --storage pg8000:host=$DB_HOST,port=$DB_PORT,database=$DB_NAME,user=$DB_USER,password=$DB_PASSWORD
